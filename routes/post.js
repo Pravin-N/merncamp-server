6ab5bf5d8b769c03f38ed01e5af1c2d99c16b9a5
Pravin N <pravin86n@gmail.com>
2022-01-28 03:01:23 +0530

    crud for posts complete

--- routes/post.js
+++ routes/post.js
@@ -1,16 +1,19 @@
 import express from "express";
-import { requireSignin } from "../middlewares";
+import { requireSignin, canEditDeletePost } from "../middlewares";
 import formidable from 'express-formidable';
 
 const router = express.Router();
 
 // controllers
-import { createPost, uploadImage, postsByUser } from "../controllers/post";
+import { createPost, uploadImage, postsByUser, userPost, updatePost, deletePost } from "../controllers/post";
 
 router.post("/create-post", requireSignin, createPost);
 router.post("/upload-image", requireSignin, formidable({maxFileSize: 5 * 1024 * 1024}), uploadImage);
 
 // posts
 router.get('/user-posts', requireSignin, postsByUser);
+router.get('/user-post/:_id', requireSignin, userPost);
+router.put('/update-post/:_id', requireSignin, canEditDeletePost, updatePost);
+router.delete('/delete-post/:_id', requireSignin, canEditDeletePost, deletePost)
 
 module.exports = router;
